1ef6f9c7186589064c4da02f553146576ddcf582
HarshSoni2002 <harshsoni12062003.com>
2023-07-16 09:50:09 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>HarshSoni</h1>
+        <p>@all right reserved</p>
+      </div>
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a
+            href="https://www.linkedin.com/in/harsh-soni-20b775223/"
+            target={"blank"}
+          >
+            linkedin
+          </a>
+          <a href="https://www.instagram.com/har__sh12/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/HarshSoni2002" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
